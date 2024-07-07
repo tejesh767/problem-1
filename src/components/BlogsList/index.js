0a5bfa083e633c7446f5e41f5e873d1499275467
tejesh767 <95077484+tejesh767@users.noreply.github.com>
2024-07-07 10:25:28 +0530

    Update index.js

--- src/components/BlogsList/index.js
+++ src/components/BlogsList/index.js
@@ -6,7 +6,9 @@ import './index.css'
 
 class BlogsList extends Component {
   state = {
+    searchInput: '',
     repositoriesData: [],
+    isSorted: false,
   }
 
   componentDidMount() {
@@ -19,20 +21,75 @@ class BlogsList extends Component {
     if (response.ok) {
       const fetchedData = await response.json()
       console.log(fetchedData)
+      const result = fetchedData.map(each => ({
+        userId: each.userId,
+        id: each.id,
+        title: each.title,
+        body: each.body,
+        mixtures: each.title + each.body,
+      }))
       this.setState({
-        repositoriesData: fetchedData,
+        repositoriesData: result,
+      })
+    }
+  }
+
+  onChangeSearchInput = event => {
+    this.setState({
+      searchInput: event.target.value,
+    })
+  }
+
+  sorting = () => {
+    const {repositoriesData, isSorted} = this.state
+
+    if (isSorted === true) {
+      const sortedData = repositoriesData.sort((p1, p2) => {
+        if (p1.title < p2.title) return 1
+        return -1
+      })
+      this.setState({
+        repositoriesData: sortedData,
+        isSorted: false,
+      })
+    } else {
+      const sortedData = repositoriesData.sort((p1, p2) => {
+        if (p1.title > p2.title) return 1
+        return -1
+      })
+      this.setState({
+        repositoriesData: sortedData,
+        isSorted: true,
       })
     }
   }
 
   render() {
-    const {repositoriesData} = this.state
+    const {searchInput, repositoriesData, isSorted} = this.state
+
+    const searchResults = repositoriesData.filter(eachUser =>
+      eachUser.mixtures.includes(searchInput),
+    )
+
+    const text = isSorted ? 'sort in DESC' : 'Sort in ASC'
+
     return (
       <div className="app-container">
         <div className="responsive-container">
           <h1 className="heading">Posts</h1>
+          <div className="line">
+            <button type="button" className="button" onClick={this.sorting}>
+              {text}
+            </button>
+            <input
+              type="search"
+              className="search"
+              onChange={this.onChangeSearchInput}
+              value={searchInput}
+            />
+          </div>
           <ul className="repositories-list">
-            {repositoriesData.map(eachRepository => (
+            {searchResults.map(eachRepository => (
               <BlogItem
                 key={eachRepository.id}
                 repositoryDetails={eachRepository}
