51eb31632e26a0052c8ebe559935c2be5f6bd204
tejesh767 <95077484+tejesh767@users.noreply.github.com>
2024-07-08 18:38:48 +0530

    Update index.js

--- src/components/BlogItem/index.js
+++ src/components/BlogItem/index.js
@@ -1,15 +1,29 @@
 // Write your code here
 // Write your code here
+import {useState, useEffect} from 'react'
 import './index.css'
 import {Link} from 'react-router-dom'
 
 const BlogItem = props => {
+  const [posts, setPosts] = useState([])
   const {repositoryDetails} = props
-  const {title, body, userId} = repositoryDetails
+  const {title, body, userId, id} = repositoryDetails
+  useEffect(() => {
+    async function fetchData() {
+      const url = `https://jsonplaceholder.typicode.com/users/${userId}`
+      const response = await fetch(url)
+      const updatedData = await response.json()
+      setPosts(updatedData.name)
+    }
+    fetchData()
+    // eslint-disable-next-line
+  }, [])
 
   return (
-    <Link to={`/blogs/${userId}`} className="repository-item">
+    <Link to={`/blogs/${id}`} className="repository-item">
       <li>
+        <h1 className="repository-name name">{posts}</h1>
+
         <h1 className="repository-name">{title}</h1>
         <div className="stats-container">
           <p className="stats-text">{body}</p>
